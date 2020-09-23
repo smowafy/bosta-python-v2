a2476330ad6be1e734a80d02e1ad78c489ad8aec
SohilaBogdadyNagdy <sohila.bogdady2012@gmail.com>
2020-09-23 17:42:07 +0200

    Get business sub accounts

--- bosta/business/Business.py
+++ bosta/business/Business.py
@@ -3,28 +3,29 @@ import requests
 import json
 import logging
 
-with open(os.path.join(os.getcwd(),'bosta-python/bosta/config.json')) as configFile:
-    config = json.load(configFile)
+from bosta.ApiClient.ApiClient import BostaClient
+
 
 class BusinessInfo:
-    apiKey = os.getenv('BOSTA_API_KEY', '')
+    def __init__(self):
+        self.apiClient = BostaClient(
+            os.environ.get('BOSTA_API_KEY'),
+            os.environ.get('BOSTA_API_BASE')
+        )
     
-    @classmethod
-    def getBusinessSubAccount(cls, pageId=0):
+    def getBusinessSubAccount(self, pageId=0):
         try:
             logging.info('Get Business Sub Account')
+            url  = self.apiClient.get_apiBase() + "businessSubAccount"
             headers = {
-                "Authorization": cls.apiKey
+                "Authorization": self.apiClient.get_apiKey()
             }
             params = {
                 "pageId": pageId 
             }
-            response = requests.get(config['BOSTA_URL'] + "businessSubAccount", params= params, headers=headers)
+            response = requests.get(url, params= params, headers=headers)
+            if (response.status_code != 200): return response.text
             return response.json()
         except Exception as exp:
             logging.error(exp)
             raise exp
-
-if __name__ == '__main__' and __package__ is None:
-    from os import sys, path
-    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
