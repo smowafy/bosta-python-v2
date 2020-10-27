730524b4267e934b45e306f2357d82d9b7015336
SohilaBogdadyNagdy <sohila.bogdady2012@gmail.com>
2020-10-27 12:33:30 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension
 
 setup(
     name='BostaSDK',
-    version='1.0.0',
+    version='1.1.0',
     author='bosta',
     author_email='sdk@bosta.com',
     scripts=[],
